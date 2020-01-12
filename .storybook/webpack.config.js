7f2e2380cd667473aa7880eb0c583277b3a1046a
sumanchalki <suman.abc@gmail.com>
2020-01-12 23:06:19 +0530

    Added and configured storybook storysource addon.

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -6,13 +6,23 @@
 // When you add this file, we won't add the default configurations which is similar
 // to "React Create App". This only has babel loader to load JavaScript.
 
-module.exports = {
-  plugins: [
-    // your custom plugins
-  ],
-  module: {
-    rules: [
-      // add your custom rules.
+const path = require('path');
+
+module.exports = async ({ config }) => {
+  config.module.rules.push({
+    test: /\.stories\.js$|\/stories\/.*\.js$/,
+    loaders: [
+      {
+        loader: require.resolve('@storybook/addon-storysource/loader'),
+        options: {
+          prettierConfig: {
+            parser: 'babel'
+          }
+        }
+      }
     ],
-  },
+    enforce: 'pre'
+  });
+
+  return config;
 };
